b70e0257176f33eb3fbb02092a853e35a1861d66
artgoblin <satabdadas3@gmail.com>
2026-05-01 09:33:56 +0530

    minor fixes

--- forgot-password/vite.config.js
+++ forgot-password/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/auth': 'http://localhost:8080',
+      '/auth': 'http://178.104.129.87:8080',
     },
   },
 })
